56841ca1c26facc9b36bff11e2c008a797109f4a
bchuinkam <bryan.chuinkam@wisewiithdata.com>
2021-04-14 23:48:40 -0400

    Added: 1) comments 2) tags to a few quotes
    Updated:
    1) printQuote() to handle tags.
    2) changeBackgroundColor() to change background color when  'show another quote' is pressed.
    3) setInterval function to change background color every 5 seconds.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 /******************************************
 Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
+Completed by Bryan Chuinkam
 ******************************************/
 
 // For assistance: 
@@ -11,34 +12,41 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
-//https://www.revolt.tv/2021/4/9/22376106/11-dmx-quotes
-//https://www.lionworldtravel.com/news/8-nelson-mandela-quotes
+// list of quotes objects
 const quotes = [
-
     {quote: "I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.",
     source: 'Maya Angelou',
     citation: '',
-    year: 0
+    year: 0,
     },
     {quote: "It always seems impossible until it's done.",
     source: 'Nelson Mandela',
     citation: '',
-    year: 2001
+    year: 2001,
+    tags: 'Politics'
     },
     {quote: "There can be no keener revelation of a society's soul than the way in which it treats its children.",
     source: 'Nelson Mandela',
     citation: "From speech Mandela gave when he launched the Children's Fund in South Africa",
-    year: 1995
+    year: 1995,
+    tags: 'Politics'
     },
     {quote: 'Education is the most powerful weapon you can use to change the world.',
     source: 'Nelson Mandela',
     citation: 'Speech delivered to the Nelson Mandela Foundation',
-    year: 2003
+    year: 2003,
+    tags: 'Politics'
     },
     {quote: "It's the tough things that we go through, hard things we go through, that get us to that point where we’re better and stronger than we've ever been.",
     source: 'DMX',
     citation: 'Wendy Williams Show',
     year: 2020
+    },
+    {quote: "Show me the money!",
+    source: 'Jerry Maguire',
+    citation: 'Movie',
+    year: 1996,
+    tags: 'Humor'
     },
 ]
 
@@ -47,6 +55,7 @@ const quotes = [
  * `getRandomQuote` function
 ***/
 function getRandomQuote(quotes){
+    // Returns random Quote object from Quotes list
     max = quotes.length
     rand_number = Math.floor(Math.random() * (max - 0) + 0)
     return quotes[rand_number]
@@ -57,6 +66,7 @@ function getRandomQuote(quotes){
  * `printQuote` function
 ***/
 function printQuote(){
+    // Returns a string of HTML text and quote properties to be displayed on page
     get_quote = getRandomQuote(quotes)
     html_string = `
     <p class="quote">${get_quote.quote}</p>
@@ -66,16 +76,34 @@ function printQuote(){
         html_string += `\n\t <span class="citation">${get_quote.citation}</span>`
         }
     if(get_quote.year){
-        html_string += `\n\t <span class="year">${get_quote.year} </span>`
+        html_string += `\n\t <span class="year">${get_quote.year}. </span>`
+    }
+    if(get_quote.tags){
+        html_string += `\n\t <span >  (${get_quote.tags}) </span>`
     }
     html_string += `\n\t</p>`
+
     document.getElementById('quote-box').innerHTML = html_string;
+    return html_string
+}
+
+function changeBackgroundColor() {
+    // changes background color to random colors of body
+    var letters = '0123456789ABCDEF'
+    var color = '#'
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+        }
+    document.body.style.backgroundColor = color
 }
 
 
+// change background color every 5 seconds
+window.setInterval(changeBackgroundColor, 5*1000)
+
+document.getElementById('load-quote').addEventListener("click", changeBackgroundColor, false);
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
-
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
